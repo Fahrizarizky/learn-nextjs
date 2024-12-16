d02bbb5a6dd5faaec3f45bbf58e6a19c18bfeb55
fahrizarizky <fahrizarizky6@gmail.com>
2024-12-16 21:26:13 +0700

    setup first

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export default function ({ children }) {
+  return (
+    <html>
+      <head lang="en"></head>
+      <body>
+        <h1> Fahriza Rizky</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
